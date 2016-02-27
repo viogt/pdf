4d683d9d0234e5d6496185fbc1d4462702939929
viorel <viorel.topa@gmail.com>
2016-02-27 17:41:01 +0000

    sead

--- server.js
+++ server.js
@@ -15,63 +15,23 @@ var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 http.createServer(function (req, res) {
 
-  if(req.method == 'GET') {
-
-    if(req.url.charAt(0) == '/') {
+    if(req.method == 'GET') {
+        
         if(req.url.slice(-5)!='.json') returnStream('.'+req.url, res);
         else operate(JSON.stringify({ collection: 'files', action: 'get', _id: 'tables' }), res);
-
-            returnFile('.'+req.url, res);
-            /*var j = require('.'+req.url);
-            res.writeHead(200, {'Content-Type': 'application/json;'});
-            res.end( JSON.stringify(j) );
-            return;
-        }*/
         return;
     }
-    else {
-        try {
-            var x = JSON.parse(decodeURI(req.url.substr(1)));
-            res.end('GET Action: ' + x.action + ' --- ' + JSON.stringify(x));
-        } catch(e) { }
-    }
-    return;
-
-  }
+    
     body = '';
     req.on('data', function (chunk) { body += chunk; });
-    req.on('end', function () { saveFile('./res/tables.json',body, res); });
+    req.on('end', function () { operate(body, res); /*saveFile('./res/tables.json',body, res);*/ });
 
 }).listen(port, ipaddress);
 
 function returnStream(fl, resp){ var file = fs.createReadStream(fl); file.pipe(resp); }
 
-function returnFile(fl, resp) { var file = fs.createReadStream(fl); file.setEncoding('utf8'); file.pipe(resp); }
-/*{
-	fs.readFile(fl, { "encoding":"utf8" },  function (err,data) {
-	  if (err) {
-		  resp.writeHead(200, { "Content-Type": "text/plain" });
-		  resp.end('0Error retreiving the file ' + fl + '...'); return;
-	  }
-	  resp.writeHead(200, {"Content-Type": "application/json; charset=utf-8"});
-	  resp.end(data, 'utf8');
-  });
-}*/
-
-function saveFile( fl, bd, resp ){
-	fs.writeFile(fl, bd, 'utf-8', function(err) {
-	  if (err) {
-		  resp.writeHead(200, {'Content-Type': 'text/plain' });
-		  resp.end('0Error writing the file.'); return;
-	  }
-	  resp.writeHead(200, {'Content-Type': 'text/plain' });
-	  resp.end('OK');
-  });
-}
-
 function operate( Rqst, resp ) {
     var js = JSON.parse(Rqst);
-    //resp.writeHead(200, {'Content-Type': 'text/plain' });
     Mng.MongoClient.connect(MngIp, function(err, db) {
         if(err) { resp.end('0 Database cannot be opened!'); return; }
         
